cc57028dc50cd1c5249870aedc82ebf932958d73
Anucha Chewachanon <68524576+SpiralNuggets@users.noreply.github.com>
2023-11-20 23:10:13 +0700

    fix: added title
    FUCK I FORGOT

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Ruka: will become TaskMate later</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
